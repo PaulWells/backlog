7850b5d7bd9330c576ecb9f6f7c05ee295f23552
Paul Wells <paulwells12@gmail.com>
2017-01-11 09:39:53 -0800

    Added basic list of items Adds the <Backlog> component which is composed of a title, n list items and an input box to add new list items.  Each list item has a checkbox to mark the item as done, text and a button to delete the item.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,136 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+
+class BackLog extends Component {
+
+  constructor () {
+    super();
+    this.state = {
+      listItems: [],
+    };
+
+    this.addLogItem = this.addLogItem.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  addLogItem (value) {
+    this.setState({ listItems: this.state.listItems.concat({text: value})});
+  }
+
+  handleDelete (index) {
+    this.setState({ listItems: this.state.listItems.filter(function (item, i) {
+      return i !== index;
+    })});
+  }
+
+  render() {
+    return (
+      <div>
+      <LogTitle/>
+      {this.state.listItems.map(function(item, i){
+        return <LogItem text={item.text} onDelete={this.handleDelete} index={i} key={i}/>;
+      }.bind(this))}
+      <LogItemInput onEnter={this.addLogItem}/>
+      </div>
+    );
+  }
+}
+
+class LogTitle extends Component {
+  render() {
+    return (
+      <div>
+      Test Log
+      </div>
+    );
+  }
+}
+
+class LogItemInput extends Component {
+
+  constructor (props) {
+    super(props);
+    this.state = {
+      value: "",
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.onKeyDown = this.onKeyDown.bind(this);
+  }
+
+  onKeyDown (event) {
+    if (event.key === 'Enter') {
+      this.props.onEnter(this.state.value);
+      this.setState({value: ""});
+    }
+  }
+
+  handleChange (event) {
+    this.setState({value: event.target.value});
+  }
+
+  render() {
+    return (
+      <div>
+      <input type="text" value={this.state.value} onChange={this.handleChange} onKeyDown={this.onKeyDown}></input>
+      </div>
+    );
+  }
+}
+
+class LogItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      index: props.index,
+      done: false,
+    }
+    this.handleCheckBoxClick = this.handleCheckBoxClick.bind(this);
+  }
+
+  handleCheckBoxClick(event) {
+    console.log("handlecheck");
+    console.log(event.target.checked);
+    this.setState({done: event.target.checked});
+  }
+
+  render() {
+    console.log(this.state.done);
+    var className = (this.state.done ? "completedItem" : "");
+    return (
+      <div>
+      <CheckBox onClick={this.handleCheckBoxClick}/>
+      <span className={className}>
+      {this.props.text}
+      </span>
+      <DeleteIcon onDelete={() => this.props.onDelete(this.state.index)}/>
+      </div>
+    );
+  }
+}
+
+class CheckBox extends Component {
+  render() {
+    return (
+      <input type="checkbox" onClick={this.props.onClick}/>
+    );
+  }
+}
+
+class DeleteIcon extends Component {
+  render() {
+    return (
+      <button onClick={this.props.onDelete}>X</button>
+    )
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BackLog/>
       </div>
     );
   }
