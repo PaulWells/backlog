53db46826a85a207087b3c989e4e06faf902dcb0
Paul Wells <paulwells12@gmail.com>
2017-01-29 12:10:52 -0800

    Added refactored container components using connect object from react-router

--- src/components/LogItemInput.js
+++ src/components/LogItemInput.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 // Presentational component which has no business logic
 const Input = ({
@@ -12,42 +13,40 @@ const Input = ({
   </div>
 );
 
-// Container component which decouples a presentational component and it's
-// parent component
-class LogItemInput extends Component {
-
-  componentDidMount() {
-    this.unsubscribe = this.context.store.subscribe(() => this.forceUpdate());
+const mapStateToProps = (state) => {
+  return {
+    value: state ? state.inputText : ""
   }
+};
 
-  componentWillUnmount() {
-    this.unsubscribe();
+const mapDispatchToProps = (dispatch) => {
+  return {
+    handleTextChange: (event) => {
+      dispatch({type: 'UPDATE_NEW_LIST_ITEM_INPUT', value: event.target.value})
+    },
+    dispatch
   }
+};
 
-  render() {
-    const { store } = this.context;
-    const state = store.getState();
+const mergeProps = (stateProps, dispatchProps, ownProps) => {
+  const { dispatch } = dispatchProps;
+  const { value } = stateProps;
 
-    function onKeyDown(event) {
+  return {
+    onKeyDown: (event) => {
       if (event.key === 'Enter') {
-        store.dispatch({type: 'ADD_LIST_ITEM', text: state.inputText});
+        dispatch({type: 'ADD_LIST_ITEM', text: value});
       }
-    }
-
-    function handleTextChange(event) {
-      store.dispatch({type: 'UPDATE_NEW_LIST_ITEM_INPUT', value: event.target.value})
-    }
-
-    return (
-      <Input
-        onKeyDown={onKeyDown}
-        handleTextChange={handleTextChange}
-        value={state ? state.inputText : ""}/>
-    );
+    },
+    handleTextChange: dispatchProps.handleTextChange,
+    ...stateProps,
   }
-}
-LogItemInput.contextTypes = {
-  store: React.PropTypes.object
-}
+};
+
+const LogItemInput = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+  mergeProps
+)(Input);
 
 export { LogItemInput }
